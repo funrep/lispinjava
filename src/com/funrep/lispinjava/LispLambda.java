8e044589bfb69123c3c56782ba05d639a1d28d53
funrep <fuuunrep@gmail.com>
2016-03-08 12:54:06 +0100

    implemented apply method for lambdas

--- src/com/funrep/lispinjava/LispLambda.java
+++ src/com/funrep/lispinjava/LispLambda.java
@@ -15,7 +15,6 @@ public class LispLambda extends LispValue {
 
 	@Override
 	LispValue eval() {
-		
 		return this;
 	}
 
@@ -26,7 +25,27 @@ public class LispLambda extends LispValue {
 
 	@Override
 	LispValue apply(List<LispValue> args) {
-		return null;
+		ArrayList<LispValue> b = applyArgs(body, args);
+		for (int i = 0; i < b.size(); i++) {
+			LispValue val = b.get(i).eval();
+			b.set(i, val);
+		}
+		return b.get(b.size() - 1);
+	}
+	
+	ArrayList<LispValue> applyArgs(ArrayList<LispValue> body,
+			List<LispValue> args) {
+		for (int i = 0; i < body.size(); i++) {
+			LispValue val = body.get(i);
+			if (val instanceof LispSymbol) {
+				LispSymbol sym = (LispSymbol) val;
+				if (params.contains(sym.symbol)) {
+					int j = params.indexOf(sym.symbol);
+					body.set(i, args.get(j));
+				}
+			}
+		}
+		return body;
 	}
 
 }
